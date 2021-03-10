d027a34d0f07e5f98ce44566eeb698220d2c3fee
Chris Coltsman <chris.coltsman@googlemail.com>
2021-03-10 20:36:12 +0100

    Initial commit

---
+++ src/main/java/io/ccoltx/spring/boot/awsecsdemo/AwsEcsDemoApplication.java
@@ -0,0 +1,13 @@
+package io.ccoltx.spring.boot.awsecsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsEcsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsEcsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/ccoltx/spring/boot/awsecsdemo/AwsEcsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ccoltx.spring.boot.awsecsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsEcsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
